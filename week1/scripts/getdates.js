b4f484c60582f2da243c660b3d85d775df0db5b7
Angela Hubbard <angiewheeler99@gmail.com>
2024-06-19 17:20:49 -0600

    Home Page

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,8 @@
+// JavaScript code to get the current year
+var currentYear = new Date().getFullYear();
+
+// Display the current year in the HTML element with id="currentYear"
+document.getElementById("currentYear").textContent = currentYear;
+
+//Display a time stamp
+alert(document.lastModified);
