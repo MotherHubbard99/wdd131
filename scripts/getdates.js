dd39c0821b8f79a9cc1679a9dfb65d475554fd72
Angela Hubbard <angiewheeler99@gmail.com>
2024-06-19 23:52:46 -0600

    changes made due to audit tool

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,43 +2,15 @@
 var currentYear = new Date().getFullYear();
 
 // Display the current year in the HTML element with id="currentYear"
-document.getElementById("currentYear").textContent = currentYear;
+document.getElementById('currentYear').textContent = currentYear;
 
-//Display a time stamp
-alert(document.lastModified);
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the <span> element where you want to display the modified date
+    var modifiedDateSpan = document.getElementById('modifiedDate');
 
-const year = document.querySelector("#year");
+    // Get the last modified date of the document
+    var lastModifiedDate = document.lastModified;
 
-// use the date object
-const today = new Date();
-
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-UK",
-    {
-        dateStyle: "full"
-    }
-).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "short"
-    }
-).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
-    "en-US",
-    {
-        dateStyle: "medium"
-    }
-).format(today)}</span>`;
-
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
-
+    // Update the content of the <span> element with the last modified date
+    modifiedDateSpan.textContent = lastModifiedDate;
+});
