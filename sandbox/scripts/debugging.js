73bfc8cf179e7e21541a3b0423364d28f3ddc1a1
Angela Hubbard <angiewheeler99@gmail.com>
2024-11-15 19:29:35 -0700

    posting sandbox learning for week 02

---
+++ sandbox/scripts/debugging.js
@@ -0,0 +1,11 @@
+const pi = 3.14;
+//will change so use let instead of const
+let area = 0;
+//calculate the area of each circle
+function circleArea(radius) {
+    const area = radius * pi;
+    return area
+}
+area = circleArea(3)
+console.log('Area2: area');
+
